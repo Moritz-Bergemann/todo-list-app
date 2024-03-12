73444795bde002d7b990248aa2e235eb974a12b0
StanleyZhong901 <stanleyzhong901@gmail.com>
2024-03-12 12:42:14 +0800

    Editted front end and added test data

--- todo-list-frontend/src/routes/+page.svelte
+++ todo-list-frontend/src/routes/+page.svelte
@@ -1,6 +1,20 @@
+<!-- 
+Things to add:
+
+1) Add feature
+2) Remove feature
+3) Edit feature
+4) Save tasks to JSON file
+5) Upload tasks to JSON file
+6) ID feature to remember user 
+-->
+
 <script lang="ts">
     let pingResponseMessage: string;
     let pingResponseCount: number;
+    let tempName = '';
+    let taskName: string;
+    let taskArray = [];
  
     async function ping() {
         // Make a GET request to the ping-pong backend API 
@@ -13,13 +27,76 @@
         pingResponseMessage = responseJson.message;
         pingResponseCount = responseJson.count;
     }
+
+    async function addTask() {
+        // Add tasks to the Backend
+        if (tempName == '') {
+            taskName = undefined;
+        }
+        else{
+            taskName = tempName;
+            taskArray.push(taskName)
+        }
+    }
+    
+    async function deleteTask() {
+        // Delete tasks to the Backend
+    }
+    
+    async function updateTask() {
+        // Update tasks to the Backend
+    }
+
+    async function saveTask() {
+        // Export tasks to the Backend
+    }
+    
 </script>
  
 <h1>POGGERS Ping Pong</h1>
 
-<button on:click={ping}>Do Ping</button>
+<div>
+    <button on:click={ping}>
+        Do Ping
+    </button>
+</div>
 
 {#if pingResponseMessage != undefined}
     <p>The ping response message was: {pingResponseMessage}</p>
     <p>The ping response count was: {pingResponseCount}</p>
 {/if}
+
+<input bind:value={tempName} placeholder="Enter Task" />
+
+<div>
+    <button on:click={addTask}>
+        Add Task
+    </button>
+</div>
+
+{#if taskName != undefined}
+    <p>The task you added was: {taskName}</p>
+{/if}
+
+{#if taskArray != undefined}
+    <p>Current Tasks: {taskArray.join(', ')}</p>
+{/if}
+
+<div>
+    <button on:click={deleteTask}>
+        Delete Task
+    </button>
+</div>
+
+<div>
+    <button on:click={updateTask}>
+        Update Task
+    </button>
+</div>
+
+<div>
+    <button on:click={saveTask}>
+        Save Task
+    </button>
+</div>
+
