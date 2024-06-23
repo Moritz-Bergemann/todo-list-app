74fd00bae75068262b936ae6939460a061492eb7
root <root@DESKTOP-PQHO6DM.localdomain>
2024-06-23 17:11:24 +0800

    completed fileIO

--- todo-list-backend/src/index.ts
+++ todo-list-backend/src/index.ts
@@ -1,18 +1,28 @@
 import express, { type Request, type Response } from 'express';
+import { readFileSync, writeFileSync } from 'fs'
 import type { TodoItem } from "./types";
-import { getElement } from './methods';
+import { saveTodoList, readTodoList } from './fileIO';
+import { error } from 'console';
 
 const app = express();
 const port = 3000;
 
-let counter = 0;
-const todoItems: TodoItem[] = [];
+let todoItems: TodoItem[] = [];
 
 app.get("/", (req: Request, res: Response) => {
     res
         .setHeader("Access-Control-Allow-Origin","*")
         .status(200)
         .send("ping recieved");
+});
+
+app.get("/refresh-todos",(req: Request, res: Response) => {
+    todoItems = readTodoList()
+    res
+        .setHeader("Access-Control-Allow-Origin","*")
+        .status(200)
+        .send(JSON.stringify(todoItems))
+    ;
 });
 
 app.get("/get-todos",(req: Request, res: Response) => {
@@ -24,14 +34,23 @@ app.get("/get-todos",(req: Request, res: Response) => {
 });
 
 app.post("/add-todo",(req: Request, res: Response) => {
+    let id
+    
+    if ( todoItems.length == 0 ) {
+        id = 0
+    }else{
+        id = todoItems[todoItems.length-1].id + 1
+    }
+
     const newToDo: TodoItem = {
-        id: counter,
+        id: id,
         name: req.body.name,
         isDone: false
     };
-    counter++;
 
     todoItems.push(newToDo);
+
+    saveTodoList(todoItems)
 
     res
         .setHeader("Access-Control-Allow-Origin","*")
@@ -45,98 +64,92 @@ app.post("/remove-todo",(req: Request, res: Response) => {
     const request = JSON.parse(req.body);
     const id = request.id;
     const name = request.name;
-    let idx = 0;
-    let subject: TodoItem;
-    try{
-        subject = getElement(todoItems, id);
-    }catch(e){
+
+    let idx = todoItems.findIndex((value, index, list) => {value.id == id})
+
+    if ( idx == -1 ) {
         res
             .setHeader("Access-Control-Allow-Origin","*")
-            .status(422)
-            .send("no task of provided id")
-        ;
-        return;
-    }
-    if ( subject.name != name ) {
-        res
-        .setHeader("Access-Control-Allow-Origin","*")
-        .status(422)
-        .send("task of given id dose not have given name")
-        ;
+            .status(400)
+            .send("no such element")
     }else{
-        let removedElement = todoItems.splice(idx, 1)[0];
+        saveTodoList(todoItems)
+        let removedElement = todoItems.splice(idx,1)[0]
         res
-        .setHeader("Access-Control-Allow-Origin","*")
-        .status(200)
-        .send(JSON.stringify(removedElement))
-        ;
+            .setHeader("Access-Control-Allow-Origin","*")
+            .status(200)
+            .send(JSON.stringify(removedElement))
     }
-
 });
 
 app.post("/tag-task-as-complete",(req: Request, res: Response) => {
     const request = JSON.parse(req.body);
     const id = request.id
     const strict = request.strict;
-
-    let subject: TodoItem;
+    let result
     try{
-        subject = getElement(todoItems, id);
+        result = changeCompletionStatusHandler(id, strict, true)
     }catch(e){
-        res
-            .setHeader("Access-Control-Allow-Origin","*")
-            .status(422)
-            .send("no task of provided id")
-        ;
-        return;
+        if ( e instanceof Error ){
+            res
+                .setHeader("Access-Control-Allow-Origin","*")
+                .status(400)
+                .send(e.message)
+            ;
+        }else{
+            res
+                .setHeader("Access-Control-Allow-Origin","*")
+                .status(500)
+                .send("unknown backend error")
+        }
+        return
     }
-    if ( subject.isDone && strict ){
-        res
-            .setHeader("Access-Control-Allow-Origin","*")
-            .status(422)
-            .send("task already complete")
-        ;
-        return;
-    }
-    subject.isDone = true;
     res
         .setHeader("Access-Control-Allow-Origin","*")
         .status(200)
-        .send(JSON.stringify(subject))
-    ;
+        .send(JSON.stringify(result))
 });
 
-app.post("/tag-task-as-incomplete", (req: Request, res: Response) => {
+app.post("/tag-task-as-incomplete",(req: Request, res: Response) => {
     const request = JSON.parse(req.body);
     const id = request.id
     const strict = request.strict;
-
-    let subject: TodoItem;
+    let result
     try{
-        subject = getElement(todoItems, id);
+        result = changeCompletionStatusHandler(id, strict, false)
     }catch(e){
-        res
-            .setHeader("Access-Control-Allow-Origin","*")
-            .status(422)
-            .send("no task of provided id")
-        ;
-        return;
+        if ( e instanceof Error ){
+            res
+                .setHeader("Access-Control-Allow-Origin","*")
+                .status(400)
+                .send(e.message)
+            ;
+        }else{
+            res
+                .setHeader("Access-Control-Allow-Origin","*")
+                .status(500)
+                .send("unknown backend error")
+        }
+        return
     }
-    if ( !subject.isDone && strict ){
-        res
-            .setHeader("Access-Control-Allow-Origin","*")
-            .status(422)
-            .send("task not yet complete complete")
-        ;
-        return;
-    }
-    subject.isDone = false;
     res
         .setHeader("Access-Control-Allow-Origin","*")
         .status(200)
-        .send(JSON.stringify(subject))
-    ;
+        .send(JSON.stringify(result))
 });
+
+function changeCompletionStatusHandler(id: number, strict: boolean, newStatus: boolean) {
+    let idx = todoItems.findIndex((value, _, __) => {value.id == id})
+
+    if ( idx == -1 ) {  throw new Error("no such element")    }
+    if ( strict && !(Number(newStatus) ^ Number()) ){   throw new Error("element is already in desired state")    }
+
+    todoItems[idx].isDone = newStatus
+
+    saveTodoList(todoItems)
+
+    return todoItems[idx]
+}
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
